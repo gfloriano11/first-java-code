075b75e0a6a7526d574c1e6ffe0904082b735a72
gfloriano11 <gflorianodev@gmail.com>
2025-08-16 22:12:08 -0300

    first code in java

---
+++ Main/src/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+
+
+    public static void main(String[] args){
+        int myAge = 18;
+        String name = "Gustavo";
+
+        System.out.println("Hello! My name is " + name + " and I'm " + myAge + " years old!");
+        System.out.println("This is my first code in Java. Trying to learn this programming language ;)");
+    }
+}
