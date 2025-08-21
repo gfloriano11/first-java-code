4f0d3ba1c212b06473a3875c93f145f5a4c36c3b
gfloriano11 <gflorianodev@gmail.com>
2025-08-21 17:11:53 -0300

    feat: add primitive vars types

---
+++ primitive-types/src/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+
+    public static void main(String[] args){
+
+        //primitive types in java:
+        byte byte_var = 8; // this type only can have values that are equal or smaller than 8 bits.
+        // -128 to 127;
+        short short_var = 32767; // this type only can have values that are between -32679 and 32768
+        int var = 18; // this type is the most common used, can have values between -2147483648 and 2147483647
+        long long_var; // used to represent numbers that are big
+
+
+    }
+}
