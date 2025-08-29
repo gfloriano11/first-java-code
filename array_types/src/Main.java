b502f077935da7ebb870434b571d55e3c6bc71bb
gfloriano11 <gflorianodev@gmail.com>
2025-08-28 21:52:38 -0300

    feat: add first code with array[

---
+++ array_types/src/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+
+    public static void main(String[] args){
+
+        // it's like in CS, where you need to declare the type in array and the var name,
+        // after that, you need to do the var receive the array with the size
+        int[] int_array_test = new int[4];
+    }
+}
