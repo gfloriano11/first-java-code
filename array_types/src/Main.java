e87383105bf3afc2ab20a62dd4ce05ebcdf296fc
gfloriano11 <gflorianodev@gmail.com>
2025-08-28 22:13:47 -0300

    feat: add ArrayList method

--- array_types/src/Main.java
+++ array_types/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main{
@@ -9,6 +10,7 @@ public class Main{
         // it's like in CS, where you need to declare the type in array and the var name,
         // after that, you need to do the var receive the array with the size
         int[] int_array_test = new int[4];
+        char[] array_test = new char[5];
 
         for(int i = 0; i < int_array_test.length; i++){
             int i_somado = i+1;
@@ -20,5 +22,21 @@ public class Main{
             int i_somado = i+1;
             System.out.println("Numero " + i_somado + ": " + int_array_test[i]);
         }
+
+        // but, when we dont know the max size of the array?
+        // like in CS too, we can use the ArrayList:
+
+        ArrayList<String> my_list = new ArrayList<>();
+        // in c++, whe use my_list.push_back(), but in Java, we use:
+
+        my_list.add("Gustavo");
+        my_list.add("Johann");
+        my_list.add("Gaucho");
+        my_list.add("VH");
+
+        System.out.println(my_list.get(1)); // print 'Johann'
+        // in Java whe use get method and array index to access the value
+        // PS: Only when we are using ArrayList!!!
+
     }
 }
