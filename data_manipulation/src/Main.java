7e220c3d42f03786e05a11b7bdbd57f62dd2e160
gfloriano11 <gflorianodev@gmail.com>
2025-08-31 15:28:12 -0300

    feat: add casting files

---
+++ data_manipulation/src/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+
+    public static void main(String[] args){
+        double resultado = 0.0;
+        int resultadoInt = (int)resultado;
+
+        int meuInt = 10;
+        double meuDouble = meuInt;
+
+        String stringNumber = "10";
+        int newInt = Integer.parseInt(stringNumber);
+
+        String myString = String.valueOf(newInt);
+    }
+}
